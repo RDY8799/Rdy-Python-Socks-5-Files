1bc4175b8bfc181036762f1b45c042ca77f7af39
RDY8799 <RDY8799@users.noreply.github.com>
2018-05-03 14:02:32 -0300

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -14,7 +14,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:' + str(RDY_PORT_SSH)
-RESPONSE = 'HTTP/1.1 200 RDY SOFTWARE - @rdysoftware'
+RESPONSE = 'HTTP/1.1 200 RDY SOFTWARE - @rdysoftware' + str(RDY_LINES)
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
 
