7e0e3b7bb7babe6eaa408ad7ffaf2053c7e08778
RDY8799 <RDY8799@users.noreply.github.com>
2018-05-03 13:43:32 -0300

    Update proxy3128.py

--- proxy3128.py
+++ proxy3128.py
@@ -12,7 +12,7 @@ PASS = ''
 # CONST
 BUFLEN = 4096 * 4
 TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:' + int(RDY_PORT_SSH)
+DEFAULT_HOST = '127.0.0.1:' + str(RDY_PORT_SSH)
 RESPONSE = 'HTTP/1.1 200 RDY SOFTWARE - @rdysoftware\r\n\r\n'
 #RESPONSE = 'HTTP/1.1 200 Hello_World!\r\nContent-length: 0\r\n\r\nHTTP/1.1 200 Connection established\r\n\r\n'  # lint:ok
 
